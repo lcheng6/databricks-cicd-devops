2a12eb8e49c416f6ae20afcfdb11c1055e3b3c64
lcheng6 <lcheng6>
2025-08-03 14:31:09 +0000

    refactor data access w/ APIs

--- notebooks/library/fetch_data_from_api.py
+++ notebooks/library/fetch_data_from_api.py
@@ -2,7 +2,18 @@ import os
 
 def get_class_data_from_api(spark):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    class_example_data_path = os.path.join(current_dir, "data/Class_Dataset.csv")
+    class_example_data_path = os.path.join(current_dir, "../data/Class_Dataset.csv")
     class_example_data_path = "file:/Workspace" + class_example_data_path
     print(class_example_data_path)
     df_class = spark.read.csv(class_example_data_path, header=True, inferSchema=True)
+    return df_class
+
+def get_score_data_from_api(spark):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    score_example_data_path = os.path.join(current_dir, "../data/Score_Dataset.csv")
+    score_example_data_path = "file:/Workspace" + score_example_data_path
+    print(score_example_data_path)
+
+    df_score = spark.read.csv(score_example_data_path, header=True, inferSchema=True)
+    return df_score
