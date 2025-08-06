2cd0617a143a1d452299cbc828496d2f87708dc9
lcheng6 <lcheng6>
2025-08-06 15:52:39 +0000

    Created result dataset for integration test / joining dataframes

--- tests/integration/api_data_fetcher_test.py
+++ tests/integration/api_data_fetcher_test.py
@@ -5,9 +5,11 @@ from unittest.mock import patch
 from pyspark.sql import SparkSession
 import pyspark as ps
 import pandas as pd
-import pyspark.sql as ps
+import pyspark as ps
 import os
 import sys
+
+from pyspark.testing import assertDataFrameEqual
 
 # Import the functions to be tested
 # from library.fetch_data_from_api import get_class_data_from_api, get_score_data_from_api
@@ -21,30 +23,22 @@ def spark():
     spark = SparkSession.builder.getOrCreate()
     return spark
 
-from library.class_business_logic import inner_join_dataframes
-def call_data_getters_and_join_dataframes(spark):
-    df_class = class_getter(spark)
-    df_score = score_getter(spark)
-    df_joined = inner_join_dataframes(df_class, df_score, "class_id")
-    return df_joined
-
 @pytest.fixture
-def spark() -> SparkSession:
-    """Fixture for creating a Spark session for testing."""
-    spark = SparkSession.builder.getOrCreate()
-    return spark
-
 def mock_successful_class_data(spark) -> ps.sql.DataFrame:
     """Fixture to provide mock class data from the class API"""
     # create the data from sample_data/Class_Dataset.csv
-    df_class = spark.read.csv("file:/Workspace/sample_data/Class_Dataset.csv", header=True, inferSchema=True)
+    class_data_path = os.path.join(CURRENT_DIR, "../sample_data/Class_Dataset.csv")
+    class_data_path = "file:/Workspace" + class_data_path
+    df_class = spark.read.csv(class_data_path, header=True, inferSchema=True)
     return df_class
 
 @pytest.fixture
 def mock_successful_score_data(spark) -> ps.sql.DataFrame:
     """Fixture to provide mock score data from the score API"""
     # create the data from sample_data/Score_Dataset.csv
-    df_score = spark.read.csv("file:/Workspace/sample_data/Score_Dataset.csv", header=True, inferSchema=True)
+    score_data_path = os.path.join(CURRENT_DIR, "../sample_data/Score_Dataset.csv")
+    score_data_path = "file:/Workspace" + score_data_path
+    df_score = spark.read.csv(score_data_path, header=True, inferSchema=True)
     return df_score
 
 @pytest.fixture
@@ -53,12 +47,12 @@ def mock_successful_joined_data(spark) -> ps.sql.DataFrame:
     df_joined = spark.read.csv("file:/Workspace/sample_data/Joined_Dataset.csv", header=True, inferSchema=True)
     return df_joined
 
-def test_inner_join_dataframes(spark, mock_successful_joined_data, mock_successful_class_data, mock_successful_score_data):
+def test_inner_join_dataframes(spark, mock_successful_class_data, mock_successful_score_data, mock_successful_joined_data):
     """Test the inner_join_dataframes function with mocked data."""
     
     # Act
-    df_joined = inner_join_dataframes(mock_successful_joined_data, mock_successful_class_data, ['class_id'])
-    # compare df_joined with mock_successful_joined_data
-    assert df_joined.count() == mock_successful_joined_data.count()
-    assert df_joined.columns == mock_successful_joined_data.columns
+    df_joined = inner_join_dataframes(mock_successful_class_data, mock_successful_score_data, ['class_id'])
+    assert(True)
+
+    assertDataFrameEqual(df_joined, mock_successful_joined_data)
 
